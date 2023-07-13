9e9b5d878471e186068eeb77545d734d8c04fb82
FlexxN1 <juandavidx40@gmail.com>
2023-07-13 18:43:37 -0500

    [ADD/UPDATE] Components and update App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import '../styles/components/app.css';
-import { BrowserRouter, Switch, Router, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from '../containers/Home';
+import Checkout from '../containers/Checkout';
+import Information from '../containers/Information';
+import Payment from '../containers/Payment';
+import Success from '../containers/Success';
+import NotFound from '../containers/NotFound';
 
-const App = () => {
+
+function App () {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={} />
-                <Route exact path="/chackout" component={} />
-                <Route exact path="/checkout/info" component={} />
-                <Route exact path="/checkout/payment" component={} />
-                <Route exact path="/checkout/success" component={} />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/checkout" component={Checkout} />
+                <Route exact path="/checkout/information" component={Information} />
+                <Route exact path="/checkout/payment" component={Payment} />
+                <Route exact path="/checkout/success" component={Success} />
+                <Route component={NotFound} />
             </Switch>
         </BrowserRouter>
     )
 }
 
-export { App }
+export default App
 
 
