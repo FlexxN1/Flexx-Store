6c2e31a46557b431d7c75ab285799bf9e9a1c90c
FlexxN1 <juandavidx40@gmail.com>
2023-07-13 20:04:07 -0500

    [ADD] Link react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 import '../styles/components/Header.css';
 
 function Header () {
     return (
         <div className="Header">
-            <h1 className="Header-title">Flexx Store</h1>
+            <h1 className="Header-title">
+                <Link to='/'>
+                    Flexx Store
+                </Link>
+            </h1>
             <div className="Header-checkout">
-            Checkout
+                <Link to='/checkout'>Checkout</Link>
             </div>
         </div>
     );
