f80f150fe9e602024164c1f8b562f83331bec6ed
FlexxN1 <juandavidx40@gmail.com>
2023-07-14 19:25:02 -0500

    [UPDATE] Checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useContext, useReducer } from 'react'
 import { Link } from 'react-router-dom';
+import { v4 as uuidv4 } from'uuid';
+import AppContext from '../context/AppContext'
 import '../styles/components/Checkout.css'
 
 function Checkout () {
-return (
+  const { state: { cart }, removeFromCart } = useContext(AppContext);
+
+  const handleRemove = (index) => {
+    removeFromCart(index)
+  }
+
+  const handleSumTotal = () => {
+    const reducer = (acumulator, currentValue) => acumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0)
+    return sum
+  }
+
+  return (
     <div className="Checkout">
       <div className="Checkout-content">
-        <h3>Lista de Pedidos:</h3>
-        <div className="Checkout-item">
-          <div className="Checkout-element">
-            <h4>ITEM name</h4>
-            <span>$10</span>
+        {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3>}
+        {cart.map((item, index) => (
+          <div className="Checkout-item" key={uuidv4()}>
+            <div className="Checkout-element">
+              <h4>{item.title}</h4>
+              <span>
+                $
+                {item.price}
+              </span>
+            </div>
+            <button type="button" onClick={() => handleRemove(index)}>
+              <i className="fas fa-trash-alt" />
+            </button>
           </div>
-          <button type="button">
-            <i className="fas fa-trash-alt" title="Eliminar"/>
-          </button>
-        </div>
+        ))}
       </div>
-      <div className="Checkout-sidebar">
-        <h3>Precio Total: $10</h3>
-        <Link to='/checkout/information'>
+      {cart.length > 0 && (
+        <div className="Checkout-sidebar">
+          <h3>{`Precio Total: $ ${handleSumTotal()}`}</h3>
+          <Link to="/checkout/information">
             <button type="button">Continuar pedido</button>
-        </Link>
-      </div>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
